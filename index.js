4d6256ae0a12d1d0994a0f2b9c874057af5e22dc
Nikita Bayev <nikita@bayev.kz>
2020-06-19 16:19:08 +0600

    Added support for HtmlWebpackPlugin version 4

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 export class BaseHrefWebpackPlugin {
   constructor(options) {
     this.options = options;
-   }
+  }
+
+  processTags(data, callback) {
+    // Check if base tag already exists
+    const baseTagRegex = /<base.*?>/i;
+    const baseTagMatches = data.html.match(baseTagRegex);
+    if (!baseTagMatches) {
+      // Insert it in top of the head
+      data.html = data.html.replace(
+        /<head>/i,
+        '$&' + `<base href="${this.options.baseHref}">`
+      );
+    } else {
+      // Otherwise replace href attribute of current base tag
+      const modifiedBaseTag = baseTagMatches[0].replace(
+        /href="\S+"/i,
+        `href="${this.options.baseHref}"`
+      );
+      data.html = data.html.replace(baseTagRegex, modifiedBaseTag);
+    }
+
+    callback(null, data);
+  }
 
   apply(compiler) {
     if (!this.options.baseHref) {
       return;
     }
 
-    compiler.hooks.compilation.tap('BaseHrefWebpackPlugin', (compilation) => {
-      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('BaseHrefWebpackPlugin', (data, callback) => {
-        // Check if base tag already exists
-        const baseTagRegex = /<base.*?>/i;
-        const baseTagMatches = data.html.match(baseTagRegex);
-        if (!baseTagMatches) {
-          // Insert it in top of the head
-          data.html = data.html.replace(/<head>/i, '$&' + `<base href="${this.options.baseHref}">`);
-        } else {
-          // Otherwise replace href attribute of current base tag
-          const modifiedBaseTag = baseTagMatches[0].replace(/href="\S+"/i, `href="${this.options.baseHref}"`);
-          data.html = data.html.replace(baseTagRegex, modifiedBaseTag);
-        }
-
-        callback(null, data);
+    // HtmlWebpackPlugin version 4.0.0-beta.5 and upper
+    if (HtmlWebpackPlugin.getHooks) {
+      compiler.hooks.compilation.tap('BaseHrefWebpackPlugin', compilation => {
+        HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync(
+          'BaseHrefWebpackPlugin',
+          this.processTags.bind(this)
+        );
       });
-    });
+    } else {
+      // HtmlWebpackPlugin version 3.2.0
+      compiler.hooks.compilation.tap('BaseHrefWebpackPlugin', compilation => {
+        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
+          'BaseHrefWebpackPlugin',
+          this.processTags.bind(this)
+        );
+      });
+    }
   }
 }
